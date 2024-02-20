2fbbe833ef20f34af1220806f8286558cd1be403
vulkz <gabrielcosta3536@gmail.com>
2024-02-20 00:17:34 -0300

    remove redundant try-catch

--- src/handlers/LinkedinHideChat.js
+++ src/handlers/LinkedinHideChat.js
@@ -2,12 +2,7 @@ async function LinkedinHideChat(window) {
     if (window.location.hostname !== "www.linkedin.com")
         return;
 
-    try{
-        let button = await Fn.waitFor(() => document.querySelector(".msg-overlay-bubble-header__controls"));
-        await Fn.sleep(500);
-        button.children[2].click();
-    }catch(err){
-        console.error(err);
-    }
-    
+    let button = await Fn.waitFor(() => document.querySelector(".msg-overlay-bubble-header__controls"));
+    await Fn.sleep(500);
+    button.children[2].click();
 }
